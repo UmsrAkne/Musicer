a0ca9dcae3c708b012006a2a70036de876d76a10
Akn <sibata486@gmail.com>
2022-06-16 00:07:06 +0900

    init

---
+++ Musicer/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Musicer.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using System.Windows;
+
+namespace Musicer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
