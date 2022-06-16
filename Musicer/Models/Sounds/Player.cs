24726b0b08d58b0987f838340383cd4a3fbad52f
Akn <sibata486@gmail.com>
2022-06-17 00:36:34 +0900

    add / サウンドを再生する機能を担うクラスを追加

---
+++ Musicer/Models/Sounds/Player.cs
@@ -0,0 +1,29 @@
+﻿namespace Musicer.Models.Sounds
+{
+    public class Player
+    {
+        private SoundProvider soundProvider;
+
+        public SoundProvider SoundProvider { get => soundProvider; set => soundProvider = value; }
+
+        public void Play()
+        {
+        }
+
+        public void Stop()
+        {
+        }
+
+        public void Pause()
+        {
+        }
+
+        public void Next()
+        {
+        }
+
+        public void Prev()
+        {
+        }
+    }
+}
