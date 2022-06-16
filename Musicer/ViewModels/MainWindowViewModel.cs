7b629c305d3779334a481bf9b05cc9ffa586ea05
Akn <sibata486@gmail.com>
2022-06-16 23:52:34 +0900

    add / TreeView で選んだ項目の内容を ListView に表示するコード

--- Musicer/ViewModels/MainWindowViewModel.cs
+++ Musicer/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,13 @@
 ﻿namespace Musicer.ViewModels
 {
     using System;
+    using System.Collections.Generic;
     using System.Collections.ObjectModel;
+    using System.IO;
+    using System.Linq;
     using System.Windows.Controls;
     using Musicer.Models.Files;
+    using Musicer.Models.Sounds;
     using Prism.Commands;
     using Prism.Mvvm;
 
@@ -13,6 +17,7 @@
 
         private ExtendFileInfo selectedDirectory;
         private ObservableCollection<ExtendFileInfo> directories = new ObservableCollection<ExtendFileInfo>();
+        private List<ISound> musics;
         private DelegateCommand<TreeView> setTreeViewSelectedItemCommand;
 
         public MainWindowViewModel()
@@ -28,11 +33,25 @@
 
         public ObservableCollection<ExtendFileInfo> Directories { get => directories; set => SetProperty(ref directories, value); }
 
+        public List<ISound> Musics { get => musics; set => SetProperty(ref musics, value); }
+
         public DelegateCommand<TreeView> SetTreeViewSelectedItemCommand
         {
             get => setTreeViewSelectedItemCommand ?? (setTreeViewSelectedItemCommand = new DelegateCommand<TreeView>((tv) =>
             {
-                selectedDirectory = tv.SelectedItem as ExtendFileInfo;
+                var directory = tv.SelectedItem as ExtendFileInfo;
+
+                if (directory == selectedDirectory)
+                {
+                    return;
+                }
+
+                selectedDirectory = directory;
+
+                if (directory.HasSoundFile)
+                {
+                    Musics = (directory.FileSystemInfo as DirectoryInfo).GetFiles().Select(f => new Sound(f) as ISound).ToList();
+                }
             }));
         }
     }
