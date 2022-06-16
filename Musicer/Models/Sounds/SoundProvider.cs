03f68faf49a2ba30bd92f5efab2ff64e68f21953
Akn <sibata486@gmail.com>
2022-06-17 00:31:30 +0900

    add / ISound を順番に取得する機能を持つクラスを追加

---
+++ Musicer/Models/Sounds/SoundProvider.cs
@@ -0,0 +1,49 @@
+﻿namespace Musicer.Models.Sounds
+{
+    using System.Collections.Generic;
+
+    public class SoundProvider
+    {
+        private List<ISound> source;
+
+        public bool LoopPlay { get; set; }
+
+        public int Index { get; private set; }
+
+        public List<ISound> Source
+        {
+            get => source;
+            set
+            {
+                source = value;
+                Index = 0;
+            }
+        }
+
+        /// <summary>
+        /// 実行する毎に Source の中のサウンドを順番に取得します。
+        /// </summary>
+        /// <returns>Source に含まれるサウンドを取得します。</returns>
+        public ISound GetSound()
+        {
+            if (Source == null || Source.Count == 0)
+            {
+                return null;
+            }
+
+            if (Source.Count < Index + 1)
+            {
+                if (!LoopPlay)
+                {
+                    return null;
+                }
+                else
+                {
+                    Index = 0;
+                }
+            }
+
+            return Source[Index++];
+        }
+    }
+}
