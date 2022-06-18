9360d672c26955b7e95ea8c2dc7dea5d4110d41a
Akn <sibata486@gmail.com>
2022-06-18 22:50:07 +0900

    add / FaderにSoundを入力するメソッドを追加

--- Musicer/Models/Sounds/Fader.cs
+++ Musicer/Models/Sounds/Fader.cs
@@ -1,9 +1,27 @@
 ﻿namespace Musicer.Models.Sounds
 {
+    using System.Collections.Generic;
+    using System.Linq;
+
     /// <summary>
     /// 複数の ISound を受け取り、内部で音量の制御を行うクラスです。
     /// </summary>
     public class Fader
     {
+        private List<ISound> sounds = new List<ISound>();
+
+        public int ManagementSoundCount => sounds.Count;
+
+        public void AddSound(ISound sound)
+        {
+            sounds.Add(sound);
+
+            while (sounds.Count > 2)
+            {
+                var s = sounds.First();
+                s.Volume = 0;
+                sounds.Remove(s);
+            }
+        }
     }
 }
---
+++ MusicerTests/Models/Sounds/FaderTests.cs
@@ -0,0 +1,26 @@
+﻿namespace Musicer.Models.Sounds.Tests
+{
+    using NUnit.Framework;
+
+    public class FaderTests
+    {
+        [Test]
+        public void AddSoundTest()
+        {
+            Fader fader = new Fader();
+            Assert.AreEqual(fader.ManagementSoundCount, 0);
+
+            fader.AddSound(new DummySound());
+            Assert.AreEqual(fader.ManagementSoundCount, 1);
+
+            fader.AddSound(new DummySound());
+            Assert.AreEqual(fader.ManagementSoundCount, 2);
+
+            fader.AddSound(new DummySound());
+            Assert.AreEqual(fader.ManagementSoundCount, 2);
+
+            fader.AddSound(new DummySound());
+            Assert.AreEqual(fader.ManagementSoundCount, 2);
+        }
+    }
+}
