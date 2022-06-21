ae527e85e7df820a55b56497455f0ec7ccc94c35
Akn <sibata486@gmail.com>
2022-06-21 16:29:41 +0900

    add / SoundViewer のテストクラスを追加

--- Musicer/Models/Sounds/SoundViewer.cs
+++ Musicer/Models/Sounds/SoundViewer.cs
@@ -6,9 +6,31 @@
     public class SoundViewer : BindableBase
     {
         private List<ISound> sounds = new List<ISound>();
+        private string playingMusicName;
+
+        public string PlayingMusicName { get => playingMusicName; set => SetProperty(ref playingMusicName, value); }
 
         public void Add(ISound sound)
         {
+            sounds.Add(sound);
+
+            while (sounds.Count > 2)
+            {
+                sounds.RemoveAt(0);
+            }
+
+            if (sounds.Count == 0)
+            {
+                PlayingMusicName = string.Empty;
+            }
+            else if (sounds.Count == 1)
+            {
+                PlayingMusicName = sounds[0].Name;
+            }
+            else
+            {
+                PlayingMusicName = $"{sounds[0].Name} >>> {sounds[1].Name}";
+            }
         }
     }
 }
---
+++ MusicerTests/Models/Sounds/SoundViewerTests.cs
@@ -0,0 +1,30 @@
+﻿namespace Musicer.Models.Sounds.Tests
+{
+    using NUnit.Framework;
+
+    public class SoundViewerTests
+    {
+        [Test]
+        [Description("サウンドを追加するテスト")]
+        public void サウンドを追加するテスト要素１()
+        {
+            var soundViewer = new SoundViewer();
+
+            soundViewer.Add(new DummySound() { Name = "test1" });
+
+            Assert.AreEqual(soundViewer.PlayingMusicName, "test1");
+        }
+
+        [Test]
+        [Description("サウンドを追加するテスト")]
+        public void サウンドを追加するテスト要素２()
+        {
+            var soundViewer = new SoundViewer();
+
+            soundViewer.Add(new DummySound() { Name = "test1" });
+            soundViewer.Add(new DummySound() { Name = "test2" });
+
+            Assert.AreEqual(soundViewer.PlayingMusicName, "test1 >>> test2");
+        }
+    }
+}
