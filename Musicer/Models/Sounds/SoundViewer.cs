b10285dce9c5e3ba708b687cde477ce4df009f5c
Akn <sibata486@gmail.com>
2022-06-21 16:14:07 +0900

    add / ビューへの表示を補助するためのクラスを追加

---
+++ Musicer/Models/Sounds/SoundViewer.cs
@@ -0,0 +1,14 @@
+﻿namespace Musicer.Models.Sounds
+{
+    using System.Collections.Generic;
+    using Prism.Mvvm;
+
+    public class SoundViewer : BindableBase
+    {
+        private List<ISound> sounds = new List<ISound>();
+
+        public void Add(ISound sound)
+        {
+        }
+    }
+}
