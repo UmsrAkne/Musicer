d973775eb4d7163c383d964f218b79dbad351df9
Akn <sibata486@gmail.com>
2022-06-24 00:47:31 +0900

    add / 指定ディレクトリの展開を補助する機能を持つクラスを追加

---
+++ Musicer/Models/Files/DirectoryExpander.cs
@@ -0,0 +1,45 @@
+﻿namespace Musicer.Models.Files
+{
+    using System.IO;
+
+    public static class DirectoryExpander
+    {
+        public static bool ExpandDirectories(ExtendFileInfo rootDirectory, DirectoryInfo targetPath)
+        {
+            if (targetPath.FullName == rootDirectory.FileSystemInfo.FullName)
+            {
+                // 目的となるパスがルートディレクトリと同じなら、ルートディレクトリだけ展開して終了
+                rootDirectory.IsExpanded = true;
+                return false;
+            }
+
+            if (!targetPath.FullName.StartsWith(rootDirectory.FileSystemInfo.FullName))
+            {
+                // 目的となるディレクトリがルートディレクトリの子孫かを確認する。
+                // たどり着けない場合(このブロックに入った場合)はこれ以上処理できないので処理を終了する。
+                return false;
+            }
+
+            var differencePath = targetPath.FullName.Substring(rootDirectory.FileSystemInfo.FullName.Length);
+            var directoryNames = differencePath.Split(new string[] { "\\" }, System.StringSplitOptions.RemoveEmptyEntries);
+
+            rootDirectory.IsExpanded = true;
+
+            var directory = rootDirectory;
+
+            foreach (var name in directoryNames)
+            {
+                directory.ChildDirectories.ForEach(d =>
+                {
+                    if (d.IsDirectory && d.Name == name)
+                    {
+                        d.IsExpanded = true;
+                        directory = d;
+                    }
+                });
+            }
+
+            return true;
+        }
+    }
+}
---
+++ MusicerTests/Models/Files/DirectoryExpanderTests.cs
@@ -0,0 +1,42 @@
+﻿namespace Musicer.Models.Files.Tests
+{
+    using System.IO;
+    using NUnit.Framework;
+
+    public class DirectoryExpanderTests
+    {
+        [Test]
+        public void ExpandDirectoriesTest()
+        {
+            var testDirectory1 = new DirectoryInfo("test");
+            testDirectory1.Create();
+
+            var testDirectory2 = new DirectoryInfo("test\\test2");
+            testDirectory2.Create();
+
+            var testDirectory22 = new DirectoryInfo("test\\test22");
+            testDirectory22.Create();
+
+            var testDirectory3 = new DirectoryInfo("test\\test2\\test3");
+            testDirectory3.Create();
+
+            var rootDirectory = new ExtendFileInfo("test");
+            var targetDirectory = new DirectoryInfo("test\\test2\\test3");
+            DirectoryExpander.ExpandDirectories(rootDirectory, targetDirectory);
+
+            Assert.AreEqual(rootDirectory.ChildDirectories[0].Name, "test2");
+            Assert.IsTrue(rootDirectory.ChildDirectories[0].IsExpanded);
+
+            Assert.AreEqual(rootDirectory.ChildDirectories[1].Name, "test22");
+            Assert.IsFalse(rootDirectory.ChildDirectories[1].IsExpanded);
+
+            Assert.AreEqual(rootDirectory.ChildDirectories[0].ChildDirectories[0].Name, "test3");
+            Assert.IsTrue(rootDirectory.ChildDirectories[0].ChildDirectories[0].IsExpanded);
+
+            testDirectory3.Delete();
+            testDirectory2.Delete();
+            testDirectory22.Delete();
+            testDirectory1.Delete();
+        }
+    }
+}
