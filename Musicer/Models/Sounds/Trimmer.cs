1fedde1b0785b087778a24c2d694ce68dc40f491
Akn <sibata486@gmail.com>
2022-07-15 00:30:24 +0900

    add / Sound の再生時間をカットするクラスを追加

---
+++ Musicer/Models/Sounds/Trimmer.cs
@@ -0,0 +1,22 @@
+﻿namespace Musicer.Models.Sounds
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// 入力されたサウンドの冒頭、末尾をカットするクラスです。
+    /// </summary>
+    public class Trimmer
+    {
+        public List<ISound> Sounds { get; set; }
+
+        public void Add(ISound sound)
+        {
+            Sounds.Add(sound);
+
+            while (Sounds.Count > 2)
+            {
+                Sounds.RemoveAt(0);
+            }
+        }
+    }
+}
