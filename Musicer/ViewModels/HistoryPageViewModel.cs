1494c2a4139f51048c9cb09c849a3895662bb774
Akn <sibata486@gmail.com>
2022-07-21 01:34:25 +0900

    add / 履歴閲覧用のページを追加

---
+++ Musicer/ViewModels/HistoryPageViewModel.cs
@@ -0,0 +1,32 @@
+﻿namespace Musicer.ViewModels
+{
+    using System;
+    using Prism.Commands;
+    using Prism.Mvvm;
+    using Prism.Services.Dialogs;
+
+    public class HistoryPageViewModel : BindableBase, IDialogAware
+    {
+        public event Action<IDialogResult> RequestClose;
+
+        public string Title => "Listen history";
+
+        public DelegateCommand CloseCommand => new DelegateCommand(() =>
+        {
+            RequestClose?.Invoke(new DialogResult());
+        });
+
+        public bool CanCloseDialog()
+        {
+            return true;
+        }
+
+        public void OnDialogClosed()
+        {
+        }
+
+        public void OnDialogOpened(IDialogParameters parameters)
+        {
+        }
+    }
+}
---
+++ Musicer/Views/HistoryPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Musicer.Views
+{
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// HistoryPage.xaml の相互作用ロジック
+    /// </summary>
+    public partial class HistoryPage : Page
+    {
+        public HistoryPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
