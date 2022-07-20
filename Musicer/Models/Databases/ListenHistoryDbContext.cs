58c912273851c08410017e694cc94725591f3c48
Akn <sibata486@gmail.com>
2022-07-21 00:40:09 +0900

    add / EntityFW を利用するための準備
    必要なパッケージをプロジェクトに追加
    必要なクラスの追加

---
+++ Musicer/Models/Databases/ListenHistoryDbContext.cs
@@ -0,0 +1,25 @@
+﻿namespace Musicer.Models.Databases
+{
+    using System.Data.SQLite;
+    using System.IO;
+    using Microsoft.Data.Sqlite;
+    using Microsoft.EntityFrameworkCore;
+
+    public class ListenHistoryDbContext : DbContext
+    {
+        private string dbFileName = "listenHistory.sqlite";
+
+        public DbSet<ListenHistory> Persons { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!File.Exists(dbFileName))
+            {
+                SQLiteConnection.CreateFile(dbFileName); // ファイルが存在している場合は問答無用で上書き。
+            }
+
+            var connectionString = new SqliteConnectionStringBuilder { DataSource = dbFileName }.ToString();
+            optionsBuilder.UseSqlite(new SQLiteConnection(connectionString));
+        }
+    }
+}
