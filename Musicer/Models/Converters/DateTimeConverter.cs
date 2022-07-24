c6223e27e00a6cfa1c5e97adf5d02ea54ebc60e8
Akn <sibata486@gmail.com>
2022-07-24 23:50:42 +0900

    add / 履歴ウィンドウの日時表示にコンバーターを適用

---
+++ Musicer/Models/Converters/DateTimeConverter.cs
@@ -0,0 +1,19 @@
+﻿namespace Musicer.Models.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    public class DateTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((DateTime)value).ToString("MM/dd HH:mm");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
