e11247bdacfc846452f8dff1da27741a90958049
Akn <sibata486@gmail.com>
2023-03-23 01:27:51 +0900

    add feat / レイアウト切り替え機能を実装

--- Musicer/Models/Layout.cs
+++ Musicer/Models/Layout.cs
@@ -4,26 +4,53 @@ namespace Musicer.Models
 {
     public class Layout : BindableBase
     {
-        private int treeViewColumnSpan = 1;
-        private int soundListColumnIndex = 1;
+        private int viewColumnSpan = 2;
+        private int viewRowSpan = 1;
+        private int soundListColumnIndex;
         private int soundListRowIndex = 2;
 
-        public int TreeViewColumnSpan
+        public int ViewColumnSpan
         {
-            get => treeViewColumnSpan;
-            set => SetProperty(ref treeViewColumnSpan, value);
+            get => viewColumnSpan;
+            private set => SetProperty(ref viewColumnSpan, value);
+        }
+
+        public int ViewRowSpan
+        {
+            get => viewRowSpan;
+            private set => SetProperty(ref viewRowSpan, value);
         }
 
         public int SoundListColumnIndex
         {
             get => soundListColumnIndex;
-            set => SetProperty(ref soundListColumnIndex, value);
+            private set => SetProperty(ref soundListColumnIndex, value);
         }
 
         public int SoundListRowIndex
         {
             get => soundListRowIndex;
-            set => SetProperty(ref soundListRowIndex, value);
+            private set => SetProperty(ref soundListRowIndex, value);
+        }
+
+        public void ToggleLayout()
+        {
+            if (ViewColumnSpan == 2)
+            {
+                // 縦分割レイアウト
+                ViewColumnSpan = 1;
+                ViewRowSpan = 2;
+                SoundListColumnIndex = 2;
+                SoundListRowIndex = 1;
+            }
+            else
+            {
+                // 横分割レイアウト
+                ViewColumnSpan = 2;
+                ViewRowSpan = 1;
+                SoundListColumnIndex = 0;
+                SoundListRowIndex = 2;
+            }
         }
     }
 }
